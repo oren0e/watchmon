e909fbf8916995304ff198fd07dfcdd8632b6fb6
oren0e <countx@gmail.com>
2022-07-08 02:54:40 +0300

    Add option for monitoring special system files like those in /proc

--- src/watching.rs
+++ src/watching.rs
@@ -1,7 +1,9 @@
 use crate::utils::look_for_term_in_file;
 use anyhow::Result as AnyhowResult;
 use anyhow::{anyhow, Context};
-use notify::{RecommendedWatcher, RecursiveMode, Watcher};
+use notify::event::{DataChange, Event, EventKind, ModifyKind};
+use notify::poll::{PollWatcher, PollWatcherConfig};
+use notify::{RecursiveMode, Watcher};
 use shlex::Shlex;
 use std::path::PathBuf;
 use std::process::Command;
@@ -10,39 +12,81 @@ use std::time::Duration;
 
 ///Monitor a file for the existance of a term, and if the term is not found in the file, trigger
 ///the command
-pub fn monitor_file_for_term(term: &str, filepath: &str, cmd: &str) -> AnyhowResult<()> {
+pub fn monitor_file_for_term(
+    term: &str,
+    filepath: &str,
+    cmd: &str,
+    monitor_fs_files: bool,
+) -> AnyhowResult<()> {
     let (tx, rx) = channel();
 
-    let mut watcher = notify::recommended_watcher(tx)?;
-    //let mut watcher = watcher(tx, Duration::from_secs(10)).expect("Failed to create watcher");
-    watcher
-        .watch(&PathBuf::from(&filepath), RecursiveMode::NonRecursive)
-        .unwrap_or_else(|_| panic!("Could not watch path {}", &filepath));
+    if monitor_fs_files {
+        let config = PollWatcherConfig {
+            compare_contents: true,
+            poll_interval: Duration::from_secs(2),
+        };
+        let mut watcher = PollWatcher::with_config(tx, config)?;
+        watcher.watch(&PathBuf::from(&filepath), RecursiveMode::NonRecursive)?;
 
-    loop {
-        match rx.recv() {
-            Ok(event) => {
-                println!("event: {:?}", event);
-                //if let DebouncedEvent::Write(path) = event {
-                //    let found = look_for_term_in_file(term, &path)?;
-                //    if !found {
-                //        let mut lex = Shlex::new(cmd);
-                //        let args = lex.by_ref().collect::<Vec<_>>();
-                //        if lex.had_error {
-                //            return Err(anyhow!("Failed to parse command"));
-                //        }
-                //        let mut comm = Command::new(&args[0]);
-                //        comm.args(&args[1..]);
-                //        let mut c = comm
-                //            .spawn()
-                //            .with_context(|| format!("Failed running {}", &args[0]))?;
-                //        c.wait()
-                //            .with_context(|| format!("Failed running {}", &args[0]))?;
-                //    }
-                //}
+        loop {
+            match rx.recv() {
+                Ok(event) => {
+                    if let Ok(Event {
+                        kind: EventKind::Modify(ModifyKind::Data(DataChange::Any)),
+                        ..
+                    }) = event
+                    {
+                        let found = look_for_term_in_file(term, &filepath)?;
+                        if !found {
+                            let mut lex = Shlex::new(cmd);
+                            let args = lex.by_ref().collect::<Vec<_>>();
+                            if lex.had_error {
+                                return Err(anyhow!("Failed to parse command"));
+                            }
+                            let mut comm = Command::new(&args[0]);
+                            comm.args(&args[1..]);
+                            let mut c = comm
+                                .spawn()
+                                .with_context(|| format!("Failed running {}", &args[0]))?;
+                            c.wait()
+                                .with_context(|| format!("Failed running {}", &args[0]))?;
+                        }
+                    }
+                }
+                Err(e) => println!("watch error: {:?}", e),
             }
-            Err(e) => println!("watch error: {:?}", e),
+        }
+    } else {
+        let mut watcher = notify::recommended_watcher(tx)?;
+        watcher.watch(&PathBuf::from(&filepath), RecursiveMode::NonRecursive)?;
+
+        loop {
+            match rx.recv() {
+                Ok(event) => {
+                    if let Ok(Event {
+                        kind: EventKind::Modify(ModifyKind::Data(DataChange::Content)),
+                        ..
+                    }) = event
+                    {
+                        let found = look_for_term_in_file(term, &filepath)?;
+                        if !found {
+                            let mut lex = Shlex::new(cmd);
+                            let args = lex.by_ref().collect::<Vec<_>>();
+                            if lex.had_error {
+                                return Err(anyhow!("Failed to parse command"));
+                            }
+                            let mut comm = Command::new(&args[0]);
+                            comm.args(&args[1..]);
+                            let mut c = comm
+                                .spawn()
+                                .with_context(|| format!("Failed running {}", &args[0]))?;
+                            c.wait()
+                                .with_context(|| format!("Failed running {}", &args[0]))?;
+                        }
+                    }
+                }
+                Err(e) => println!("watch error: {:?}", e),
+            }
         }
     }
-    Ok(())
 }
