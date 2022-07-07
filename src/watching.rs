9c6d523a1633ec0b868c7d85dd2ad9bd4fd10780
oren0e <countx@gmail.com>
2022-07-08 01:20:04 +0300

    WIP: Use higher major version of notify

--- src/watching.rs
+++ src/watching.rs
@@ -1,8 +1,9 @@
 use crate::utils::look_for_term_in_file;
 use anyhow::Result as AnyhowResult;
 use anyhow::{anyhow, Context};
-use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use notify::{RecommendedWatcher, RecursiveMode, Watcher};
 use shlex::Shlex;
+use std::path::PathBuf;
 use std::process::Command;
 use std::sync::mpsc::channel;
 use std::time::Duration;
@@ -12,33 +13,36 @@ use std::time::Duration;
 pub fn monitor_file_for_term(term: &str, filepath: &str, cmd: &str) -> AnyhowResult<()> {
     let (tx, rx) = channel();
 
-    let mut watcher = watcher(tx, Duration::from_secs(10)).expect("Failed to create watcher");
+    let mut watcher = notify::recommended_watcher(tx)?;
+    //let mut watcher = watcher(tx, Duration::from_secs(10)).expect("Failed to create watcher");
     watcher
-        .watch(&filepath, RecursiveMode::NonRecursive)
+        .watch(&PathBuf::from(&filepath), RecursiveMode::NonRecursive)
         .unwrap_or_else(|_| panic!("Could not watch path {}", &filepath));
 
     loop {
         match rx.recv() {
             Ok(event) => {
-                if let DebouncedEvent::Write(path) = event {
-                    let found = look_for_term_in_file(term, &path)?;
-                    if !found {
-                        let mut lex = Shlex::new(cmd);
-                        let args = lex.by_ref().collect::<Vec<_>>();
-                        if lex.had_error {
-                            return Err(anyhow!("Failed to parse command"));
-                        }
-                        let mut comm = Command::new(&args[0]);
-                        comm.args(&args[1..]);
-                        let mut c = comm
-                            .spawn()
-                            .with_context(|| format!("Failed running {}", &args[0]))?;
-                        c.wait()
-                            .with_context(|| format!("Failed running {}", &args[0]))?;
-                    }
-                }
+                println!("event: {:?}", event);
+                //if let DebouncedEvent::Write(path) = event {
+                //    let found = look_for_term_in_file(term, &path)?;
+                //    if !found {
+                //        let mut lex = Shlex::new(cmd);
+                //        let args = lex.by_ref().collect::<Vec<_>>();
+                //        if lex.had_error {
+                //            return Err(anyhow!("Failed to parse command"));
+                //        }
+                //        let mut comm = Command::new(&args[0]);
+                //        comm.args(&args[1..]);
+                //        let mut c = comm
+                //            .spawn()
+                //            .with_context(|| format!("Failed running {}", &args[0]))?;
+                //        c.wait()
+                //            .with_context(|| format!("Failed running {}", &args[0]))?;
+                //    }
+                //}
             }
             Err(e) => println!("watch error: {:?}", e),
         }
     }
+    Ok(())
 }
